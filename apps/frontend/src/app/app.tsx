3588c2d896ad196ec280e23131bf9d19dfbf57ef
praveenganeshcp <praveenganesh7@gmail.com>
2023-06-28 23:35:13 +0530

    refactor(app): remove boilerplate code in app component

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,16 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
-import { ReactUiKit } from "@praveenkumarcp/reacteasy";
 
 export function App() {
   return (
     <div>
       <h1>hello</h1>
-      <ReactUiKit />
     </div>
   );
 }
