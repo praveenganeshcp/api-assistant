55ef560d4adcb0d337fd34de975e22cf23e4647d
praveenganeshcp <praveenganesh7@gmail.com>
2023-06-29 10:52:42 +0530

    feat(accounts): implemet login and signup ui

--- apps/frontend/src/modules/accounts/components/signup/Signup.tsx
+++ apps/frontend/src/modules/accounts/components/signup/Signup.tsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+import { Input, Button } from "@praveenkumarcp/reacteasy";
+import "./Signup.scss";
+
 export function Signup() {
     return (
-        <h1>signup page</h1>
+        <div 
+            className="h-100 w-100 d-flex flex-column justify-content-center align-items-center">
+            <form className="signup-form d-flex flex-column justify-content-between">
+                <h3 className="align-self-start">Welcome</h3>
+                <Input id="emailId" type="email" placeholder="Email ID" />
+                <Input id="username" type="text" placeholder="User name" />
+                <Input id="password" type="password" placeholder="Password" />
+                <Button className="mt-3" group="primary" label="CREATE ACCOUNT" />
+                <span className="align-self-center">
+                    Already have an account? 
+                    <Link to={"/accounts/login"}> Login here</Link>
+                </span>
+            </form>
+        </div>
     )
 }
