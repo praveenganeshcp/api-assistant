4b36c6a8f6953719affe8416126f76e5fd5278f0
Praveen Kumar <praveenganesh7@gmail.com>
2023-06-30 20:31:39 +0530

    fix(reactkit): extend prop types from react and fix styles (#5)

--- libs/react-ui-kit/src/lib/components/input/Input.tsx
+++ libs/react-ui-kit/src/lib/components/input/Input.tsx
@@ -1,13 +1,17 @@
-import { ReactEasyComponentProps } from "../../types/prop-types";
+import { InputHTMLAttributes } from "react";
 import "./Input.scss";
 
-interface InputProps extends ReactEasyComponentProps {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export function Input(props: InputProps) {
     return (
-        <input 
-            {...props}
-            className="react-easy-input"
-        />
+        <div className="react-easy-input-container">
+            {props.id && props.placeholder && <label htmlFor={props.id}>{ props.placeholder }</label>}
+            <input  
+                {...props}
+                placeholder=""
+                className={`react-easy-input ${props.className}`}
+            />
+        </div>
     )
 }
