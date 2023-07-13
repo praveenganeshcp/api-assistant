d7d897e4c483d40341f5e7a28f85de6246c2a421
Praveen Kumar <praveenganesh7@gmail.com>
2023-07-13 22:05:44 +0530

    feat(login): integrate login flow (#22)

--- apps/frontend/src/modules/accounts/accounts.api.ts
+++ apps/frontend/src/modules/accounts/accounts.api.ts
@@ -3,3 +3,7 @@ import { axiosHttpClient } from "../shared/config/axios.config";
 export function fetchUserProfile() {
     return axiosHttpClient.get("accounts/profile").then(res => res.data);
 }
+
+export function loginAccount(emailId: string, password: string) {
+    return axiosHttpClient.post("accounts/login", {emailId, password});
+}
--- apps/frontend/src/modules/accounts/components/login/Login.tsx
+++ apps/frontend/src/modules/accounts/components/login/Login.tsx
@@ -1,19 +1,34 @@
 import { Link } from "react-router-dom";
 import "./Login.scss";
 import { Input, Button } from "@praveenkumarcp/reacteasy";
+import { useState } from "react";
+import { loginAccount } from "../../accounts.api";
+import { loadUserProfile } from "../../auth-user.slice";
+import { useDispatch } from "react-redux";
 
 export function Login() {
+
+    const [emailId, setEmailId] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+
+    const onLoginAccount = async (e: any) => {
+        e.preventDefault();
+        await loginAccount(emailId, password)
+        dispatch<any>(loadUserProfile());
+    }
+
     return (
         <div 
             className="h-100 w-100 d-flex flex-column justify-content-center align-items-center">
-            <form className="login-form d-flex flex-column justify-content-between">
+            <form onSubmit={onLoginAccount} className="login-form d-flex flex-column justify-content-between">
                 <h3 className="align-self-start">Welcome back!</h3>
-                <Input id="emailId" type="email" placeholder="Email ID" />
+                <Input id="emailId" onChange={(e) => setEmailId(e.target.value)} type="email" placeholder="Email ID" />
                 <div className="d-flex flex-column">
-                    <Input id="password" type="password" placeholder="Password" />
+                    <Input onChange={(e) => setPassword(e.target.value)} id="password" type="password" placeholder="Password" />
                     <Link className="mt-1" to={''}>Forgot Password ?</Link>
                 </div>
-                <Button group="primary" label="LOGIN" />
+                <Button group="primary" type="submit" label="LOGIN" />
                 <span className="align-self-center">
                     Do not have an account? 
                     <Link to={"/accounts/signup"}> Signup here</Link>
