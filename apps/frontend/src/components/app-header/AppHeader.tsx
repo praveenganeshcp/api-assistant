2f04b26f5d5d586a5f922c49f23f37825ee5be49
praveenganeshcp <praveenganesh7@gmail.com>
2023-07-14 19:28:12 +0530

    feat(logout): implement logout api and integrate in fe

--- apps/frontend/src/components/app-header/AppHeader.tsx
+++ apps/frontend/src/components/app-header/AppHeader.tsx
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { APP_CONFIG } from "../../constants";
 import "./AppHeader.scss";
+import { Button } from "@praveenkumarcp/reacteasy";
+import { axiosHttpClient } from "../../modules/shared/config/axios.config";
+import { useDispatch } from "react-redux";
+import { resetState } from "../../modules/shared/models/entity-state.model";
 
 export function AppHeader() {
+
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+
+    async function onLogout() {
+        await axiosHttpClient.post("/accounts/logout");
+        dispatch(resetState())
+        navigate("/accounts/login");
+    }
+
     return (
         <header className="api-assistant-app-header">
             <Link to="/app/dashboard">{APP_CONFIG.NAME}</Link>
+            <Button onClick={onLogout} label="Logout" group="text" />
         </header>
     )
 }
