456cb9c913954306e84711b1f5bcf166c7726757
Praveen Kumar <praveenganesh7@gmail.com>
2023-07-20 21:58:11 +0530

    feat(modal): implement a basic modal component (#27)

---
+++ libs/react-ui-kit/src/lib/components/modal/modal.stories.tsx
@@ -0,0 +1,22 @@
+import { Modal } from "./Modal";
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+    title: 'Modal',
+    component: Modal
+} as Meta
+
+type Story = StoryObj<typeof Modal>;
+
+export const Primary: Story = {
+    args: {
+        isOpen: true,
+        cancelText: "Cancel",
+        confirmText: "Confirm",
+        onCancel: () => {console.log('on cancel')},
+        onConfirm: () => {console.log('on confirm')},
+        children: <span>Hello</span>,
+        title: "Add Modal title"
+    }
+}
+Primary.storyName = "Modal"
