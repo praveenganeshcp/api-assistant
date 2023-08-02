067d24feb80917808b1c74005e424b18203ba0a7
praveenganeshcp <praveenganesh7@gmail.com>
2023-08-02 22:30:19 +0530

    feat(storybook): add a11y addon

--- libs/react-ui-kit/.storybook/main.ts
+++ libs/react-ui-kit/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx)', '../src/lib/**/*.mdx'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/react-vite',
     options: {
