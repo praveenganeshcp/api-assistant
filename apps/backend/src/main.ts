bf695cc6bca28cf5b2f324d83f5730f3ff9e7fa1
praveenganeshcp <praveenganesh7@gmail.com>
2023-08-19 14:23:57 +0530

    fix(cors): read site host from env

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,15 +6,18 @@ import { AppModule } from "./modules/app/app.module"
 import { useContainer } from 'class-validator';
 import { appConfig } from './config/app.config';
 import cookieParser from "cookie-parser";
-import cors, {CorsOptions} from "cors";
+import cors from "cors";
 
 async function bootstrap() {
   // bootstrap app module
   const app = await NestFactory.create(AppModule);
 
+  // set app port
+  const applicationConfig: ConfigType<typeof appConfig> = app.get(appConfig.KEY)
+
   // cookie parser middleware
   app.use(cookieParser(), cors({
-    origin: "http://localhost:4200",
+    origin: applicationConfig.FE_HOST_ADDRESS,
     credentials: true    
   }))
 
@@ -28,8 +31,6 @@ async function bootstrap() {
   // class-validator DI settings
   useContainer(app.select(AppModule), {fallbackOnErrors: true});
 
-  // set app port
-  const applicationConfig: ConfigType<typeof appConfig> = app.get(appConfig.KEY)
   await app.listen(applicationConfig.PORT);
 
   Logger.log(
