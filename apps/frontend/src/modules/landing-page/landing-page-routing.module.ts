568739f2244a1c8476636a06dae062340b7e3af3
praveenganeshcp <praveenganesh7@gmail.com>
2023-09-17 09:18:41 +0530

    feat(landing-page): setup routes and component

---
+++ apps/frontend/src/modules/landing-page/landing-page-routing.module.ts
@@ -0,0 +1,9 @@
+import { Route } from "@angular/router";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
+
+export const landingPageRoutes: Route[] = [
+    {
+        path: "",
+        component: LandingPageComponent
+    }
+]
