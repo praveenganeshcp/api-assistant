06b46364e9bf1a6cbeb260db05504017e47d5983
Praveen Kumar <praveenganesh7@gmail.com>
2023-09-29 18:59:38 +0530

    feat(project-details): create ui for project details page (#40)
    * feat(project-details): create ui for project details page
    
    * fix(actions): remove lint step in workflow

---
+++ apps/frontend/src/modules/project-details/components/project-home/project-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { SwIconComponent } from "ngx-simple-widgets";
+
+@Component({
+	selector: 'api-assistant-project-home',
+	standalone: true,
+	templateUrl: './project-home.component.html',
+	styleUrls: ['./project-home.component.scss'],
+	imports: [SwIconComponent]
+})
+export class ProjectHomeComponent {}
