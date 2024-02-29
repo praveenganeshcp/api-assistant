c57d24d80fe796fa22f5f3a19383216f7d9d9b00
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-02-29 23:13:35 +0530

    refactor(app-header): fix app header

--- apps/frontend/src/modules/app-shell/components/app-shell/app-shell.component.ts
+++ apps/frontend/src/modules/app-shell/components/app-shell/app-shell.component.ts
@@ -2,25 +2,25 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { RouterModule } from '@angular/router';
-import { logoutAccount } from '../../../accounts/store/actions';
-import { isProfileVerifiedSelector } from '../../../accounts/store/selectors';
+import { logoutAccountAction } from '@api-assistant/auth-fe';
+import { isUserProfileVerifiedSelector } from '@api-assistant/auth-fe';
 import { Observable } from 'rxjs';
 import { SwButtonComponent, SwIconComponent } from 'ngx-simple-widgets';
 
 @Component({
-  selector: 'api-assistant-app-header',
-  templateUrl: './app-header.component.html',
-  styleUrls: ['./app-header.component.scss'],
+  selector: 'api-assistant-app-shell',
+  templateUrl: './app-shell.component.html',
+  styleUrls: ['./app-shell.component.scss'],
   standalone: true,
   imports: [RouterModule, CommonModule, SwButtonComponent, SwIconComponent],
 })
-export class AppHeaderComponent {
+export class AppShellComponent {
   constructor(private store: Store<any>) {}
 
   public handleLogout() {
-    this.store.dispatch(logoutAccount());
+    this.store.dispatch(logoutAccountAction());
   }
 
   public isAccountVerified$: Observable<boolean | undefined> =
-    this.store.select(isProfileVerifiedSelector);
+    this.store.select(isUserProfileVerifiedSelector);
 }
