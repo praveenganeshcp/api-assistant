f96ce551323e53535e4b2967744a953c7638e6cc
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-02-29 21:01:36 +0530

    refactor(config): add deleted files

--- apps/backend/src/config/db.config.ts
+++
@@ -1,7 +0,0 @@
-import { registerAs } from "@nestjs/config";
-import { ENV_VARIABLES } from "./env.validator";
-
-export const dbConfig = registerAs('database', () => ({
-    DB_URL: ENV_VARIABLES.DB_URL,
-    DB_NAME: ENV_VARIABLES.DB_NAME
-}))
--- apps/backend/src/config/email.config.ts
+++
@@ -1,7 +0,0 @@
-import { registerAs } from "@nestjs/config";
-import { ENV_VARIABLES } from "./env.validator";
-
-export const emailconfig = registerAs('email', () => ({
-    USER: ENV_VARIABLES.EMAIL_AUTH_USER,
-    PASS: ENV_VARIABLES.EMAIL_AUTH_PASS
-}))
