2d9d473e2eaacbe6af646e3c2da053315e42ad47
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-03-06 06:16:15 +0530

    feat(project-overview): bind project overview data

--- apps/frontend/src/modules/project-details/components/project-home/project-home.component.ts
+++ apps/frontend/src/modules/project-details/components/project-home/project-home.component.ts
@@ -1,11 +1,26 @@
 import { Component } from "@angular/core";
+import { Store } from "@ngrx/store";
 import { SwIconComponent } from "ngx-simple-widgets";
+import { AppState } from "../../../app/app.state";
+import { projectOverviewSelector } from "../../store/selectors";
+import { AsyncPipe, NgIf } from "@angular/common";
 
 @Component({
 	selector: 'api-assistant-project-home',
 	standalone: true,
 	templateUrl: './project-home.component.html',
 	styleUrls: ['./project-home.component.scss'],
-	imports: [SwIconComponent]
+	imports: [
+		SwIconComponent,
+		NgIf,
+		AsyncPipe
+	]
 })
-export class ProjectHomeComponent {}
+export class ProjectHomeComponent {
+
+	public readonly projectOverview$ = this.store.select(projectOverviewSelector);
+
+	constructor(
+		private store: Store<AppState>
+	) {}
+}
