e4f3937eee2e3b9aad3488a4b00c5efe453987af
Praveen Kumar <praveenganesh7@gmail.com>
2024-03-06 06:17:26 +0530

    feat(project-overview): bind project overview data (#71)
    Co-authored-by: Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>

--- apps/frontend/src/modules/project-details/store/selectors.ts
+++ apps/frontend/src/modules/project-details/store/selectors.ts
@@ -12,3 +12,38 @@ export const projectDetailsErrorSelector = createSelector(
     projectDetailsSelector,
     (projectDetailsState) => projectDetailsState.error
 )
+
+const projectDataSlice = createSelector(
+    projectDetailsSelector,
+    (projectDetailsState) => projectDetailsState.data
+)
+
+export const projectOverviewSelector = createSelector(
+    projectDataSlice,
+    (projectData) => {
+        if(projectData == null) {
+            return null;
+        }
+        const dbActions = projectData.metadata.count;
+        return {
+            id: projectData._id,
+            name: projectData.name,
+            createdOn: projectData.createdOn,
+            dbStats: {
+                ...dbActions,
+                totalActions: (
+                    dbActions.createAction +
+                    dbActions.updateAction +
+                    dbActions.readAction +
+                    dbActions.deleteAction +
+                    dbActions.aggregate
+                ),
+                totalCollections: 0
+            },
+            storage: projectData.metadata.storage,
+            noOfFiles: projectData.metadata.noOfFiles,
+            totalUsers: projectData.metadata.users
+        }
+        
+    }
+)
