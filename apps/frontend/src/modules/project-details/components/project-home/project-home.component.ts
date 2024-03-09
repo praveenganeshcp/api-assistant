e4f7fb43c7d5bbbe250edcb84f7f1b41b4f0a48a
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-03-09 22:40:45 +0530

    refactor(project-home): fix ui responsive

--- apps/frontend/src/modules/project-details/components/project-home/project-home.component.ts
+++ apps/frontend/src/modules/project-details/components/project-home/project-home.component.ts
@@ -3,7 +3,10 @@ import { Store } from "@ngrx/store";
 import { SwIconComponent } from "ngx-simple-widgets";
 import { AppState } from "../../../app/app.state";
 import { projectOverviewSelector } from "../../store/selectors";
-import { AsyncPipe, NgIf } from "@angular/common";
+import { AsyncPipe, NgFor, NgIf } from "@angular/common";
+import { Observable, map } from "rxjs";
+import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
+import { NgChartsModule } from 'ng2-charts';
 
 @Component({
 	selector: 'api-assistant-project-home',
@@ -13,12 +16,58 @@ import { AsyncPipe, NgIf } from "@angular/common";
 	imports: [
 		SwIconComponent,
 		NgIf,
-		AsyncPipe
+		AsyncPipe,
+		NgFor,
+		NgChartsModule
 	]
 })
 export class ProjectHomeComponent {
 
-	public readonly projectOverview$ = this.store.select(projectOverviewSelector);
+	private readonly projectOverview$ = this.store.select(projectOverviewSelector);
+
+	public lineChartData$: Observable<ChartConfiguration<'bar'>['data']> = this.projectOverview$.pipe(
+		map(projectData => {
+			if(projectData === null) {
+				return {
+					labels: [] as string[],
+					datasets: [
+						{
+							data: [] as number[],
+							label: ''
+						}
+					]
+				}
+			}
+			const dbStats = projectData.dbStats;
+			return {
+				labels: [
+				  'Create',
+				  'Read',
+				  'Update',
+				  'Delete',
+				  'Aggregate'
+				],
+				datasets: [
+				  {
+					data: [
+						10 || dbStats.createAction,
+						20 ?? dbStats.readAction,
+						30 ?? dbStats.updateAction,
+						15 ?? dbStats.deleteAction,
+						20 ?? dbStats.aggregate
+					],
+					label: 'Actions',
+					backgroundColor: 'rgba(255,0,0,0.3)'
+				  }
+				]
+			  }
+		})
+	);
+
+	public lineChartOptions: ChartOptions<'bar'> = {
+		responsive: true
+	};
+
 
 	constructor(
 		private store: Store<AppState>
