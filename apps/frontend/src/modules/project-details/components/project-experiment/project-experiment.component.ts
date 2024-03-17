933457bf644bfb112947b318428d5f8ab3ab285a
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-03-17 08:29:54 +0530

    feat(experiment): add support for file upload

--- apps/frontend/src/modules/project-details/components/project-experiment/project-experiment.component.ts
+++ apps/frontend/src/modules/project-details/components/project-experiment/project-experiment.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { environment } from 'apps/frontend/src/environments/environment.dev';
 import { ProjectDetailsRepository } from '../../repository/project-details.repository';
@@ -9,7 +9,7 @@ import { AppState } from '../../../app/app.state';
 import { projectAPIKeyDetailsSelector } from '../../store/selectors';
 import { filter, switchMap, take } from 'rxjs';
 import { AsyncPipe, NgIf } from '@angular/common';
-import { SwButtonComponent, SwInputComponent } from 'ngx-simple-widgets';
+import { CanBeNull, SwButtonComponent, SwInputComponent } from 'ngx-simple-widgets';
 
 @Component({
   selector: 'api-assistant-project-experiment',
@@ -33,7 +33,7 @@ export class ProjectExperimentComponent {
   public responseEditorOptions = new JsonEditorOptions();
 
   public get crudUrl() {
-    return `${this.apiUrl}api/v6/core-engine/crud`;
+    return `${this.apiUrl}api/v6/core-engine/${this.enableFileUploadMode ? "files" : "crud"}`;
   }
 
   input: string = '';
@@ -42,11 +42,35 @@ export class ProjectExperimentComponent {
 
   response: Object = {};
 
+  filePath: string = "";
+
+  file: CanBeNull<File> = null;
+
+  enableFileUploadMode: boolean = false;
+
+  handleModeSelection() {
+    this.cd.detectChanges() 
+  }
+
   apiKeyDetails$ = this.store.select(projectAPIKeyDetailsSelector);
+
+  get contentType(): string {
+    return this.enableFileUploadMode ? "form-data": "application/json"
+  }
+
+  handleFileSelection(event: any) {
+    const files: FileList = event.target?.files;
+    if(!files.length) {
+      return;
+    }
+    const file = files.item(0);
+    this.file = file;
+  }
 
   constructor(
     private repository: ProjectDetailsRepository,
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private cd: ChangeDetectorRef
   ) {
     this.editorOptions = new JsonEditorOptions();
     this.editorOptions.mode = 'code';
@@ -62,11 +86,26 @@ export class ProjectExperimentComponent {
       return;
     }
     this.data = value;
-    console.log(this.data);
   }
 
   sendRequest() {
     this.response = {};
+    if(this.enableFileUploadMode && this.file) {
+      const formData = new FormData();
+      formData.append(this.filePath, this.file);
+      this.apiKeyDetails$
+      .pipe(
+        filter((details) => details !== null),
+        switchMap((details) => {
+          return this.repository.uploadFile(formData, details?.key ?? "");
+        }),
+        take(1)
+      )
+      .subscribe((response) => {
+        this.response = response;
+      });
+      return;
+    }
     this.apiKeyDetails$
       .pipe(
         filter((details) => details !== null),
