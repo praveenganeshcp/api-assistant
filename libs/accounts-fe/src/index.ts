44b32a003c8dd1d81df52a37a5733c7bdccc1f11
Praveen Kumar <praveenganesh7@gmail.com>
2024-04-12 23:56:14 +0530

    feat(landing-page): implement profile loading in landing page (#92)
    * feat(landing-page): load profile in landing page
    
    * fix(imports): remove failed imports
    
    ---------
    
    Co-authored-by: Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>

--- libs/accounts-fe/src/index.ts
+++ libs/accounts-fe/src/index.ts
@@ -1,5 +1,3 @@
-export * from './lib/components/signup-form/signup-form';
-export * from './lib/components/login-form/login-form';
 export { login, fetchProfile, logout } from "./lib/repository";
 export { ProfileContext } from "./lib/providers/profile.provider";
 export * from "./lib/domain";
