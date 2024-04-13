0a96d7ea85e0011513352151a6c562a5ecd35f9e
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-04-13 23:56:20 +0530

    Revert "feat(accounts): setup store (#91)"
    This reverts commit 75e098971c6a3c5bd2ac2cbedcffab4cea523e4e.

--- apps/react-frontend/src/main.tsx
+++ apps/react-frontend/src/main.tsx
@@ -1,17 +1,14 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { Provider } from "react-redux";
 import { RouterProvider } from 'react-router-dom';
+
 import { router } from './routes';
-import { store } from './store/app.store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
--- apps/react-frontend/src/pages/accounts/store/type.ts
+++
@@ -1,7 +0,0 @@
-import { UserProfile } from "@api-assistant/accounts-fe";
-
-export interface ProfileState {
-    loading: boolean;
-    data: null | UserProfile,
-    error: string
-}
