3a91595ba299f9903ed2d70d9ae6fce0500da076
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-04-13 06:48:55 +0530

    fix(landing-page): show logout in header in authenticated

--- libs/landing-page-fe/src/lib/components/landing-page-header/landing-page-header.tsx
+++ libs/landing-page-fe/src/lib/components/landing-page-header/landing-page-header.tsx
@@ -1,19 +1,23 @@
 import styles from './landing-page-header.module.scss';
 import { Link } from 'react-router-dom';
-import { Button } from '@praveenkumarcp/reacteasy';
 
 /* eslint-disable-next-line */
 export interface LandingPageHeaderProps {
   isUserLoggedIn: boolean;
+  handleLogout: () => void
 }
 
 export function LandingPageHeader(props: LandingPageHeaderProps) {
   return (
     <header className={styles['landing-page-header']}>
       {props.isUserLoggedIn ? (
-        <Link data-testid="header-dashboard-link" to={'/app/dashboard'}>
-          Dashboard
+        <>
+         <Link data-testid="header-dashboard-link" to={'/app/dashboard'}>
+          Go to Dashboard
         </Link>
+        <button onClick={props.handleLogout}>Logout</button>
+        </>
+       
       ) : (
         <>
           <button data-testid="header-login-btn">Login</button>
