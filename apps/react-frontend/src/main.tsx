c34daefd14cd80f0335f2c4bfb6ad1d0f973242e
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-04-14 13:46:10 +0530

    chore(deploy): add test job to deploy workflow

--- apps/react-frontend/src/main.tsx
+++ apps/react-frontend/src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { Provider } from "react-redux";
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { store } from './store/app.store';
+import { ReduxContextProvider } from './store/app.state';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <Provider store={store}>
+    <ReduxContextProvider store={store}>
       <RouterProvider router={router} />
-    </Provider>
+    </ReduxContextProvider>
   </StrictMode>
 );
