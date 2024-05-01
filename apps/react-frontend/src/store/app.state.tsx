73c353ea1d618755c717376268358e871c6fb67c
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-05-01 18:27:25 +0530

    revert(deploy): revert react deploy changes

--- apps/react-frontend/src/store/app.state.tsx
+++
@@ -1,9 +0,0 @@
-import { store } from './app.store';
-import { Provider } from 'react-redux';
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-export const ReduxContextProvider = (props: any) => {
-  return <Provider store={props.store}>{props.children}</Provider>;
-};
