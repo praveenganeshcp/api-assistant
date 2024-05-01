7a1f87e7d8bb335d98e442e44fb6f29e85b58d75
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-05-01 18:14:23 +0530

    Revert "Revert "Revert "fix(landing-page): show logout in header in authenticated (#93)"""
    This reverts commit 9415e7be4597043a6218a99ca5494ca140f5b00c.

--- libs/landing-page-fe/src/lib/components/landing-page-header/landing-page-header.tsx
+++ libs/landing-page-fe/src/lib/components/landing-page-header/landing-page-header.tsx
@@ -1,23 +1,19 @@
 import styles from './landing-page-header.module.scss';
 import { Link } from 'react-router-dom';
+import { Button } from '@praveenkumarcp/reacteasy';
 
 /* eslint-disable-next-line */
 export interface LandingPageHeaderProps {
   isUserLoggedIn: boolean;
-  handleLogout: () => void
 }
 
 export function LandingPageHeader(props: LandingPageHeaderProps) {
   return (
     <header className={styles['landing-page-header']}>
       {props.isUserLoggedIn ? (
-        <>
-         <Link data-testid="header-dashboard-link" to={'/app/dashboard'}>
-          Go to Dashboard
+        <Link data-testid="header-dashboard-link" to={'/app/dashboard'}>
+          Dashboard
         </Link>
-        <button onClick={props.handleLogout}>Logout</button>
-        </>
-       
       ) : (
         <>
           <button data-testid="header-login-btn">Login</button>
