4bf07e4679c4b296215f08e557d619d4826da87e
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-05-01 18:15:31 +0530

    Revert "Revert "Revert "feat(landing-page): implement profile loading in landing page (#92)"""
    This reverts commit 5d9893b32b241d82786555aebbb40746ec7d2bda.

--- apps/react-frontend/src/routes.tsx
+++ apps/react-frontend/src/routes.tsx
@@ -1,16 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { App } from './app/App';
 import LandingPage from './pages/landing-page/landing-page';
 
 export const router = createBrowserRouter([
   {
-    path: "",
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: <LandingPage />,
-      },
-    ]
-  }
+    path: '',
+    element: <LandingPage />,
+  },
 ]);
--- libs/accounts-fe/src/index.ts
+++ libs/accounts-fe/src/index.ts
@@ -1,3 +1,5 @@
+export * from './lib/components/signup-form/signup-form';
+export * from './lib/components/login-form/login-form';
 export { login, fetchProfile, logout } from "./lib/repository";
 export { ProfileContext } from "./lib/providers/profile.provider";
 export * from "./lib/domain";
