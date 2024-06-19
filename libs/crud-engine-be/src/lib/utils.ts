1bef4ed4cd9d20f30f85ffda8a432db5a3944184
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-06-19 21:23:18 +0530

    update(crud-engine): remove db url

--- libs/crud-engine-be/src/lib/utils.ts
+++ libs/crud-engine-be/src/lib/utils.ts
@@ -6,15 +6,13 @@ const filesSeperator = '/';
 
 export const CORE_ENGINE_UPLOAD_ROOT = 'core-engine-uploads';
 
-export async function crudDbConnectionFactory(projectId: string): Promise<{
+export async function crudDbConnectionFactory(projectId: string, url: string): Promise<{
   db: Db;
   connection: MongoClient;
 }> {
   const logger = new Logger(crudDbConnectionFactory.name);
   logger.log('Connecting to crud mongodb host...');
-  const mongoClient = new MongoClient(
-    'mongodb+srv://praveenganesh7:KYWxtPJ8NWDC1A4f@api-assistant.pmoxs3i.mongodb.net/?retryWrites=true&w=majority'
-  );
+  const mongoClient = new MongoClient(url);
   const connection = await mongoClient.connect();
   logger.log('Connected to crud mongodb host');
 
