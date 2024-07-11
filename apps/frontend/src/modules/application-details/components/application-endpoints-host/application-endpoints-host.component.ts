3b17b04bb1bebaca8fbf35d2aac47778d01c1303
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-07-11 06:07:10 +0530

    feat(endpoints): show endpoints list view

--- apps/frontend/src/modules/application-details/components/application-endpoints-host/application-endpoints-host.component.ts
+++ apps/frontend/src/modules/application-details/components/application-endpoints-host/application-endpoints-host.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { fetchAllEndpoints } from '../../store/actions';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { EndpointsListViewComponent, MinimalEndpointInfo } from '@api-assistant/endpoints-fe';
+import { allEndpointsDataSelector, allEndpointsErrorSelector, allEndpointsLoadingSelector } from "../../store/selectors"
+import { AppState } from '../../../app/app.state';
+import { SwButtonComponent } from 'ngx-simple-widgets';
 
 @Component({
   selector: 'api-assistant-application-endpoints-host',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EndpointsListViewComponent, RouterModule, SwButtonComponent],
   templateUrl: './application-endpoints-host.component.html',
   styleUrls: ['./application-endpoints-host.component.scss'],
 })
-export class ApplicationEndpointsHostComponent {}
+export class ApplicationEndpointsHostComponent implements OnInit {
+
+  public readonly allEndpoints$: Observable<MinimalEndpointInfo[]> = this.store.select(allEndpointsDataSelector).pipe(
+    map(endpoints => {
+      if(endpoints === null || endpoints === undefined) {
+        return []
+      }
+      return endpoints;
+    })
+  )
+
+  public readonly endpointsLoading$: Observable<boolean> = this.store.select(allEndpointsLoadingSelector);
+
+  public readonly endpointsFetchError$: Observable<string> = this.store.select(allEndpointsErrorSelector);
+
+  constructor(
+    private readonly store: Store<AppState>,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly router: Router
+  ) {}
+
+  private get applicationId(): string {
+    return this.activatedRoute.snapshot.params['applicationId'];
+  }
+
+  ngOnInit(): void {
+      this.store.dispatch(fetchAllEndpoints({ applicationId: this.applicationId }))
+  }
+
+  public handleAddEndpoint() {
+    this.router.navigate(['app', 'projects', this.applicationId, 'endpoints', 'create'])
+  }
+  
+}
