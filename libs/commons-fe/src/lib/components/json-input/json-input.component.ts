0225d3288ebab26d70d48338f277679881a8d104
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-07-11 05:54:48 +0530

    update(commons): add json input component

---
+++ libs/commons-fe/src/lib/components/json-input/json-input.component.ts
@@ -0,0 +1,78 @@
+import { AfterViewInit, ChangeDetectionStrategy, Component, OnDestroy, ViewChild, forwardRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { JsonEditorComponent, JsonEditorOptions, NgJsonEditorModule } from 'ang-jsoneditor';
+import { CanBeNull } from 'ngx-simple-widgets';
+import { BehaviorSubject, skip, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'commons-fe-json-input',
+  standalone: true,
+  imports: [CommonModule, NgJsonEditorModule, ReactiveFormsModule],
+  templateUrl: './json-input.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => JsonInputComponent),
+      multi: true
+    }
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class JsonInputComponent implements ControlValueAccessor, AfterViewInit, OnDestroy {
+
+  @ViewChild(JsonEditorComponent) jsonEditorComponent: CanBeNull<JsonEditorComponent> = null;
+
+  public editorOptions = this.getEditorOptions();
+
+  private onChange: CanBeNull<any> = null;;
+
+  private onTouched: CanBeNull<any> = null;
+
+  public formControl = new FormControl();
+
+  private destroy$ = new BehaviorSubject(null);
+
+  ngAfterViewInit(): void {
+    if(this.jsonEditorComponent) {
+      (this.jsonEditorComponent as any).editor.aceEditor.renderer.setShowGutter(false)
+    }
+    this.formControl.valueChanges.pipe(
+      takeUntil(this.destroy$.pipe(skip(1)))
+    ).subscribe((value: any) => {
+      this.onChange(value);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(null);
+  }
+
+  writeValue(value: any): void {
+    this.formControl.setValue(value)
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  private getEditorOptions(): JsonEditorOptions {
+    const editorOptions = new JsonEditorOptions();
+    editorOptions.mode = "code";
+    editorOptions.indentation = 2;
+    editorOptions.mainMenuBar = false;
+    editorOptions.navigationBar = false;
+    editorOptions.statusBar = false;
+    (editorOptions as any).showErrorTable = false;
+    return editorOptions;
+  }
+  
+}
