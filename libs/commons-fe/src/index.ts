3b4cce4667814afba3127e1fa52650d8aa7b7900
Praveen Kumar <praveenganesh7@gmail.com>
2024-07-11 05:55:44 +0530

    update(commons): add json input component (#111)
    Co-authored-by: Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>

--- libs/commons-fe/src/index.ts
+++ libs/commons-fe/src/index.ts
@@ -2,3 +2,4 @@ export { API_BASE_URL } from './lib/tokens';
 export { StoreActionDispatcher } from './lib/StoreActionDispatcher';
 export { BreakPointObserver } from './lib/services/breakpointobserver.service';
 export { AppInfoService } from './lib/services/app-info.service';
+export * from "./lib/components/json-input/json-input.component";
---
+++ libs/commons-fe/src/lib/components/json-input/json-input.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { JsonInputComponent } from './json-input.component';
+
+describe('JsonInputComponent', () => {
+  let component: JsonInputComponent;
+  let fixture: ComponentFixture<JsonInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [JsonInputComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(JsonInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
